67e389022aca6bf1ff7971c7eb5118fd1542ebcd
David Doherty <froztydavie@unknownd2f446507e6a.home>
2026-01-22 15:50:47 -0800

    reliably time keeping

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import timersReducer from '../features/timers/TimerSlice';
+import { loadState, saveState } from "./localStorage";
+
+const persistedState = loadState();
 
 export const store = configureStore({
     reducer: {
         timers: timersReducer,
     },
+    preloadedState: persistedState,
+});
+
+store.subscribe(() => {
+    const state = store.getState();
+    saveState({timers: state.timers });
 });
